e73be8d6da6de3958071654b76254db987ce2c43
Pavan <pavan.baburao@gmail.com>
2016-03-02 22:39:11 -0800

    ParChecker

--- library/stack.py
+++ library/stack.py
@@ -2,11 +2,17 @@ class Stack:
     def __init__(self):
 	self.items = []
 
-    def push (self, item):
+    def push(self, item):
         self.items.append(item)
 
     def pop(self):
        return self.items.pop()
 
     def isEmpty(self):
-       return (self.items == [])
+       return self.items == []
+
+    def peek(self):
+	return self.items[len(self.items) - 1]
+
+    def size(self):
+	return len(self.items)
